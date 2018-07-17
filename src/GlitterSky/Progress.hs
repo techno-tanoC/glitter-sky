3819956f2a7b59f20e9fb830b4782e1b9436dc8e
techno-tanoC <develop.tanoc@gmail.com>
2018-07-17 18:30:39 +0900

    update Progress

--- src/GlitterSky/Progress.hs
+++ src/GlitterSky/Progress.hs
@@ -1,22 +1,36 @@
 module GlitterSky.Progress where
 
 import Control.Concurrent.MVar
+import qualified Data.ByteString as BS
+import Data.Char (chr)
 import Data.Int
+import Data.Word (Word8)
+import qualified Data.Map.Strict as Map
+import Network.HTTP.Types.Header as H
+import qualified Safe as S
 
-data Container = Container {
-  contentLength :: Integer,
-  aquired :: Integer
+data Progress = Progress {
+  contentLength :: Int,
+  aquired :: Int
 } deriving Show
 
-type Progress = MVar Container
+newProgress :: Progress
+newProgress = Progress 0 0
 
-newProgress :: IO Progress
-newProgress = newMVar $ Container 0 0
-
-setCL :: Progress -> Integer -> IO ()
+setCL :: MVar Progress -> Int -> IO ()
 setCL pg i = modifyMVarMasked_ pg $ \c ->
   return $ c { contentLength = i }
 
-progress :: Progress -> Integer -> IO ()
+progress :: MVar Progress -> Int -> IO ()
 progress pg i = modifyMVarMasked_ pg $ \c ->
   return $ c { aquired = aquired c + i }
+
+findCL :: H.ResponseHeaders -> Maybe Int
+findCL hs = do
+    bs <- find hs
+    S.readMay . bytesToString . BS.unpack $ bs
+  where
+    find :: H.ResponseHeaders -> Maybe BS.ByteString
+    find = Map.lookup H.hContentLength . Map.fromList
+    bytesToString :: [Word8] -> String
+    bytesToString = map (chr . fromIntegral)
