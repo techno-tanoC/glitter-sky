73b961d167c6b45d7ef7331ccfa6de14765f2c80
techno-tanoC <develop.tanoc@gmail.com>
2018-07-22 22:04:45 +0900

    update Progress

--- src/GlitterSky/Progress.hs
+++ src/GlitterSky/Progress.hs
@@ -3,12 +3,13 @@ module GlitterSky.Progress where
 import Control.Concurrent.MVar
 
 data Progress = Progress {
+  name :: String,
   contentLength :: Int,
   aquired :: Int
 } deriving Show
 
-newProgress :: Progress
-newProgress = Progress 0 0
+newProgress :: String -> Progress
+newProgress n = Progress n 0 0
 
 setCL :: MVar Progress -> Int -> IO ()
 setCL pg i = modifyMVarMasked_ pg $ \c ->
