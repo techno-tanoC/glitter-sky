7eaed6a71da7b1fa1947159d9ab97f2648694526
techno-tanoC <develop.tanoc@gmail.com>
2018-07-22 19:22:51 +0900

    introduce scotty

---
+++ src/GlitterSky.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
+module GlitterSky where
+
+import qualified GlitterSky.File as File
+import qualified GlitterSky.HTTP as HTTP
+import qualified GlitterSky.Progress as P
+import qualified GlitterSky.Renamer as Renamer
+import qualified GlitterSky.Tracker as T
+
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.MVar
+import qualified Data.ByteString as BS
+import qualified System.IO as IO
+import Network.HTTP.Client
+
+processChunk :: MVar P.Progress -> IO.Handle -> BS.ByteString -> IO ()
+processChunk pg h bs = do
+  P.progress pg $ BS.length bs
+  BS.hPut h bs
+  -- print pg
+
+download :: Manager
+         -> String
+         -> MVar P.Progress
+         -> Request
+         -> (Renamer.Name, Renamer.Ext)
+         -> IO ()
+download mngr dest pg req (name, ext) = do
+  withResponse req mngr $ \res -> do
+    case HTTP.findCL res of
+      Just cl -> P.setCL pg cl
+      Nothing -> return ()
+
+    File.actBinaryTempFile
+      (\path handle -> HTTP.readAll (responseBody res) $ processChunk pg handle)
+      (\path -> Renamer.copy path (dest, name, ext))
+  return ()
+
+startDownload :: Manager -> T.Trackers P.Progress -> IO ()
+startDownload mngr trackers = do
+  let dest = "/sky"
+  let req = ""
+
+  -- pg <- newMVar P.newProgress
+  -- download mngr dest pg req ("test", "mp4")
+
+  T.startTracker trackers P.newProgress $ \pg -> do
+    download mngr dest pg req ("test", "mp4")
+
+  forM_ [1..30] $ \_ -> do
+    T.collect trackers >>= print
+    threadDelay (1 * 1000 * 1000)
+
+  return ()
--- src/Main.hs
+++ src/Main.hs
@@ -1,59 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified GlitterSky.File as File
-import qualified GlitterSky.HTTP as HTTP
-import qualified GlitterSky.Progress as P
-import qualified GlitterSky.Renamer as Renamer
+import GlitterSky
 import qualified GlitterSky.Tracker as T
 
-import Control.Monad
-import Control.Concurrent
-import Control.Concurrent.MVar
-import qualified Data.ByteString as BS
-import qualified System.IO as IO
-import Network.HTTP.Client
 import Network.HTTP.Client.TLS (newTlsManager)
-
-processChunk :: MVar P.Progress -> IO.Handle -> BS.ByteString -> IO ()
-processChunk pg h bs = do
-  P.progress pg $ BS.length bs
-  BS.hPut h bs
-  -- print pg
-
-download :: Manager
-         -> String
-         -> MVar P.Progress
-         -> Request
-         -> (Renamer.Name, Renamer.Ext)
-         -> IO ()
-download mngr dest pg req (name, ext) = do
-  withResponse req mngr $ \res -> do
-    case HTTP.findCL res of
-      Just cl -> P.setCL pg cl
-      Nothing -> return ()
-
-    File.actBinaryTempFile
-      (\path handle -> HTTP.readAll (responseBody res) $ processChunk pg handle)
-      (\path -> Renamer.copy path (dest, name, ext))
-  return ()
+import Network.Wai.Middleware.Static
+import Web.Scotty
 
 main :: IO ()
 main = do
   mngr <- newTlsManager
   trackers <- T.newTrackers
 
-  let dest = "/sky"
-  let req = ""
-
-  -- pg <- newMVar P.newProgress
-  -- download mngr dest pg req ("test", "mp4")
-
-  id <- T.startTracker trackers P.newProgress $ \pg -> do
-    download mngr dest pg req ("test", "mp4")
-
-  forM_ [1..30] $ \_ -> do
-    T.collect trackers >>= print
-    threadDelay (1 * 1000 * 1000)
-
-  return ()
+  scotty 8888 $ do
+    get "/" $ text "hello scotty"
